e570cb5661c2c0c30795ee542ce831a71faa438e
AdamS <aschrofel10@gmail.com>
2026-03-23 14:44:05 -0500

    restarting source from exercism

--- solutions/java/two-fer/1/src/main/java/Twofer.java
+++
@@ -1,6 +0,0 @@
-public class Twofer {
-    public String twofer(String name) {
-        String outputName = (name == null) ? "you" : name;
-        return "One for " + outputName + ", one for me.";
-    }
-}
